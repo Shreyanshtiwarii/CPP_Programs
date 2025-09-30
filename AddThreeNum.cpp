df3e91d28108afd1efeb9964755217c1ef0211ea
Shreyanshtiwarii <engineershreyanshh@gmail.com>
2025-09-30 23:20:06 +0530

    Added addition of three numbers C++ program

---
+++ AddThreeNum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+//WAP to add three numbers entered by user
+int main()
+{
+    float a,b,c;
+    cout<<"Enter three numbers: "<<endl;
+    cin>>a>>b>>c;
+    cout<<"The sum of numbers is: "<<a+b+c<<endl;
+    return 0;
+}
