95b0443541f2892952a2b9bdb3c3f4088edb665a
Shreyanshtiwarii <engineershreyanshh@gmail.com>
2025-10-01 23:39:01 +0530

    Swapping of two numbers without  temporary varibale C++ program

---
+++ SwapTwoNum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+//WAP to swap two numbers without using a temporary variablee
+int main()
+{
+    int a,b;
+    cout<<"Enter the value of a and b : "<<endl;
+    cin>>a>>b;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<"After swapping the values are"<<endl<<" a is: "<<a<<" and "<<"b: "<<b;
+    return 0;
+}
