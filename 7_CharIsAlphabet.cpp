933c77fb610b890cf4836c51fbf58658a48ebc4a
Shreyanshtiwarii <engineershreyanshh@gmail.com>
2025-10-02 00:02:34 +0530

    The given character is alphabet or not C++ program

---
+++ 7_CharIsAlphabet.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+//WAP to find whether the given  character is alphabet or not
+int main()
+{
+    char ch;
+    cout<<"Enter an alphabet: ";
+    cin>>ch;
+    if(ch>='a' && ch<='z' || ch>='A' && ch<='Z')
+    {
+        cout<<ch<<" "<<"is an alphabet";
+    }
+    else{
+        cout<<ch<<" "<<"is not an alphabet";
+    }
+    return 0;
+}
