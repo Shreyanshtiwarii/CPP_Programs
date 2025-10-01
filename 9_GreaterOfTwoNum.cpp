1dab06b2853f0468f9864d73a8f4db6300832450
Shreyanshtiwarii <engineershreyanshh@gmail.com>
2025-10-02 00:08:01 +0530

    Find greater of two numbers C++ program

---
+++ 9_GreaterOfTwoNum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+//WAP to find greater of two numbers
+int main()
+{
+    int a,b;
+    cout<<"Enter a number a and b: "<<endl;
+    cin>>a>>b;
+    if (a>b)
+    {
+        cout<<"a is greater";
+    }
+    else
+    {
+        cout<<"b is greater";
+    }
+    return 0;
+}
