081cccb9f0c31e355d39112001d20fe03a58fb76
Shreyanshtiwarii <engineershreyanshh@gmail.com>
2025-10-01 23:32:59 +0530

    Swapping of two numbers using temporary variable C++ program

---
+++ SwapTwoNumTemp.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+//WAP to swap two numbers using a temporary variable
+int main()
+{
+    int a,b,temp;
+    cout<<"enter two numbers"<<endl;
+    cin>>a>>b;
+    cout<<"Before swapping"<<endl<<"a = "<<a<<endl<<"b = "<<b<<endl;
+    temp=b;
+    b=a;
+    a=temp;
+    cout<<"After swapping"<<endl<<"a = "<<a<<endl<<"b = "<<b<<endl;
+    return 0;
+}
